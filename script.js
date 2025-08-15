af4907b70d4c806e300f22fd6816f294f0e3bf53
iiNATHALIASILVAii <gabriellynathalia@outlook.com>
2025-08-15 00:18:43 -0300

    Inicio js

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1. Gerar numero aleatorio
+const numeroSecreto = Math.floor(Math.random() * 100 + 1);
+
+// 2. Controlar o num de tentativas do usuario
+/* Variaveis */
+let tentativas = 0;
+const maxTentativas = 10;
+
+// 3. Vinculo HTML
+const chuteInput = document.getElementById("chuteInput");
+const chuteBotao = document.getElementById("chuteBotao");
+const menssagem = document.getElementById("menssagem");
+const alertaTentativas = document.getElementById("alertaTentativas");
+
+// 4. Mostra num de tentativas
+alertaTentativas.textContent = `Tentativas restantes: ${
+  maxTentativas - tentativas
+}`;
+
+// 5. Função para chute
+
